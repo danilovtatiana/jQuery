82b106e1f476d7315b07b310577bbadf00153c78
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-02-09 15:40:59 +0200

    on(), events and animations: fadeout, slideUp

--- jQuery_events.js
+++ jQuery_events.js
@@ -18,3 +18,16 @@ $('input').eq(0).keypress(function(){
     $('h2').toggleClass('turnBlue')
   }
 })
+
+//on() = addEventListener
+
+$('h1').on('dblclick', function(){
+  $(this).toggleClass('turnRed');
+})
+
+//events and animations
+
+$('input').eq(1).on('click', function(){
+  // $('.container').fadeOut(3000);
+  $('.container').slideUp(3000);
+})
