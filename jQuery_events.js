3980638bbe94839acf8c6a8f44366576df41316e
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-02-09 15:18:08 +0200

    jQuery events: clicks

---
+++ jQuery_events.js
@@ -0,0 +1,11 @@
+$('h1').click(function(){
+  // console.log("There was a click!");
+  //change the heading every time you click on it
+  $(this).text("I was changed!")
+})
+
+//grabbing multiple elements
+
+$('li').click(function(){
+  console.log('any li was clicked');
+})
